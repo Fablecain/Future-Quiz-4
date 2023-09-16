aa40b390348f3119f0a4710d9d449f409a46c342
allyw <cain6gates@gmail.com>
2023-09-16 15:25:23 -0400

    Added my own starter code

---
+++ Assets/node.js
@@ -0,0 +1,44 @@
+// Initialize variables to manage quiz state, timer, and questions.
+let time;
+let timer;
+let currentQuestionIndex;
+let questions;
+
+// TODO: Initialize `time`, `currentQuestionIndex`, and `questions` array with questions and answers
+
+/**
+ * Starts the quiz by initializing the timer and loading the first question.
+ */
+function startQuiz() {
+  // TODO: Initialize the timer and update it every second
+  // TODO: Load the first question
+}
+
+/**
+ * Loads and displays the current question and possible answers.
+ */
+function loadQuestion() {
+  // TODO: Display the current question and possible answers
+  // TODO: Add event listeners to the answer buttons
+}
+
+/**
+ * Checks the given answer against the correct answer and proceeds to the next question.
+ * 
+ * @param {string} answer The answer provided by the user
+ */
+function checkAnswer(answer) {
+  // TODO: Check if the provided answer is correct
+  // TODO: Update the timer if necessary
+  // TODO: Load the next question or end the quiz
+}
+
+/**
+ * Ends the quiz, stops the timer, and allows the user to save their initials and score.
+ */
+function endQuiz() {
+  // TODO: Stop the timer
+  // TODO: Show the user their score and prompt for initials
+}
+
+// TODO: Add event listener to the "Start Quiz" button
